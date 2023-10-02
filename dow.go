cb9c2265b10ff97a3c651cb167674363d5edbdd7
Umi Uyura <umiuyura@gmail.com>
2023-10-02 23:24:04 +0900

    📝 Add package and function comments

--- dow.go
+++ dow.go
@@ -1,5 +1,7 @@
 // Dow package provide functions to get the name of the day of the week.
 //
+// The locales supported by this package depend on github.com/goodsign/monday.
+// https://pkg.go.dev/github.com/goodsign/monday#pkg-constants
 package dow
 
 import (
@@ -9,24 +11,34 @@ import (
 	"github.com/goodsign/monday"
 )
 
-// Get the name of the day of the week for a given date
+// Get the name of the day of the week for a given date.
+//
+// Return the name of the day of the week in en_US locale.
 func GetName(date time.Time) string {
 	return GetLocaleName(date, "")
 }
 
 // Get the locale name of the day of the week for a given date
+//
+// If the specified locale is not supported,
+// returns the name of the day of the week in the en_US locale.
 func GetLocaleName(date time.Time, lang string) string {
 	var locale = getLocale(lang)
 	return format(date, locale, false)
 }
 
-// Get the name of the day of the week for a given date with the date
+// Get the name of the day of the week for a given date with the date.
+//
+// Return the name of the day of the week in en_US locale.
 func GetNameWithDate(date time.Time) string {
 	var locale = getLocale("")
 	return format(date, locale, true)
 }
 
 // Get the locale name of the day of the week for a given date with the date
+//
+// If the specified locale is not supported,
+// returns the name of the day of the week in the en_US locale.
 func GetLocaleNameWithDate(date time.Time, lang string) string {
 	var locale = getLocale(lang)
 	return format(date, locale, true)
